81d2449aa10bae883f5518cacf24e73b5d3aba04
Nischay <nischay.dtu@gmail.com>
2024-09-09 23:09:27 +0530

    monorepo setup

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import { add } from "@autospace/sample-lib";
 
 export default function Home() {
   return (
-    <div>ParkEase Home</div>
+    <div>ParkEase Home {add(10, 5)}</div>
   );
 }
