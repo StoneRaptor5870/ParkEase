3d34880ed07c676bf4be6a61f373165a7352baf7
Nischay <nischay.dtu@gmail.com>
2024-09-10 16:44:19 +0530

    deps

--- apps/api/src/users/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { UsersService } from './users.service'
-import { UsersResolver } from './users.resolver'
-import { Prismaservice } from 'src/util/prisma'
-
-@Module({
-  providers: [UsersResolver, UsersService, Prismaservice],
-})
-export class UsersModule {}
--- apps/api/src/util/prisma.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, OnModuleInit } from '@nestjs/common'
-import { PrismaClient } from '@prisma/client'
-
-@Injectable()
-export class Prismaservice extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect()
-  }
-}
