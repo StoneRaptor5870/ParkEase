ceb1df8c27286969f4a6b571a7675195540a41f8
Nischay <nischay.dtu@gmail.com>
2024-09-10 14:00:33 +0530

    added validate script

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,5 @@
-import { add } from "@autospace/sample-lib";
+import { add } from '@autospace/sample-lib'
 
 export default function Home() {
-  return (
-    <div>ParkEase Home {add(10, 5)}</div>
-  );
+  return <div>ParkEase Home {add(10, 5)}</div>
 }
