91472adf62365865791585f605bb960a68895e4a
Nischay <nischay.dtu@gmail.com>
2024-09-23 16:33:57 +0530

    search garages

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import { add } from '@autospace/sample-lib'
 import { useQuery } from '@apollo/client'
-import { CompaniesDocument } from '@autospace/network/src/gql/generated'
+import { CompaniesDocument, SearchGaragesDocument, } from '@autospace/network/src/gql/generated'
 import { useSession, signOut } from 'next-auth/react'
 import Link from 'next/link'
 import { Button } from '@autospace/ui/components/atoms/Button'
@@ -11,22 +10,23 @@ import { Sidebar } from '@autospace/ui/components/organisms/Sidebar'
 export default function Home() {
   const { data, loading } = useQuery(CompaniesDocument)
   const { data: sessionData, status } = useSession()
+  const { data: garages } = useQuery(SearchGaragesDocument, {
+    variables: {
+      dateFilter: { end: '2024-12-14', start: '2024-12-04' },
+      locationFilter: {
+        ne_lat: 1,
+        ne_lng: 1,
+        sw_lat: -1,
+        sw_lng: -1,
+      },
+    },
+  })
 
   if (loading) return <div>Loading...</div>
 
   return (
-    <main className="bg-primary">
-      <div>
-        {sessionData?.user?.uid ? (
-          <Button onClick={() => signOut()}>Signout</Button>
-        ) : (
-          <Link href="/login">Login</Link>
-        )}
-      </div>
-      <div className="p-12">
-        <Sidebar>sdasdsdsad</Sidebar>
-      </div>
-      ParkEase Home {add(10, 5)}
+    <main className="bg-primary h-screen w-screen">
+      ParkEase Home 
       <div>
         {data?.companies && data.companies.length > 0 ? (
           data.companies.map((company) => (
@@ -37,6 +37,15 @@ export default function Home() {
           ))
         ) : (
           <div>No companies</div>
+        )}
+      </div>
+      <div>
+        {garages?.searchGarages && garages.searchGarages.length > 0 ? (
+          garages?.searchGarages.map((garage) => (
+            <pre key={garage.id}>{JSON.stringify(garage, null, 2)}</pre>
+          ))
+        ) : (
+          <div>No garages</div>
         )}
       </div>
     </main>
