acd2bd6c633206d08eb3a34b59ae6ed74c9f6752
Nischay <nischay.dtu@gmail.com>
2024-09-25 05:09:56 +0530

    slot booking

---
+++ apps/web/app/booking-failed/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <div>Booking Failed</div>
+  )
+}
--- libs/forms/src/searchGarages.tsx
+++ libs/forms/src/searchGarages.tsx
@@ -4,60 +4,64 @@ import { toLocalISOString } from '@autospace/util/date'
 import { ReactNode } from 'react'
 import { DefaultValues, useForm, FormProvider, Form } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { isEndTimeValid, isStartTimeValid } from './util'
+
 const minMaxTuple = z.tuple([z.number(), z.number()])
+
 export const formSchemaSearchGarage = z.object({
   startTime: z.string(),
   endTime: z.string(),
+
   locationFilter: z.object({
     ne_lat: z.number(),
     ne_lng: z.number(),
     sw_lat: z.number(),
     sw_lng: z.number(),
   }),
+
   types: z.nativeEnum(SlotType).array(),
+
   pricePerHour: minMaxTuple.optional(),
   height: minMaxTuple.optional(),
   width: minMaxTuple.optional(),
   length: minMaxTuple.optional(),
+
   skip: z.number().optional(),
   take: z.number().optional(),
 })
+
 export type FormTypeSearchGarage = z.infer<typeof formSchemaSearchGarage>
-const isStartTimeValid = (data: FormTypeSearchGarage) => {
-  const startDate = new Date(data.startTime)
-  const currentDate = new Date()
-  return startDate > currentDate
-}
-const isEndTimeValid = (data: FormTypeSearchGarage) => {
-  const startDate = new Date(data.startTime)
-  const endDate = new Date(data.endTime)
-  return endDate > startDate
-}
+
 formSchemaSearchGarage
-  .refine(isStartTimeValid, {
+  .refine(({ endTime, startTime }) => isStartTimeValid(startTime), {
     message: 'Start time should be greater than current time',
     path: ['startTime'],
   })
-  .refine(isEndTimeValid, {
+  .refine(({ endTime, startTime }) => isEndTimeValid({ endTime, startTime }), {
     message: 'End time should be greater than start time',
     path: ['endTime'],
   })
+
 export const getCurrentTimeAndOneHourLater = () => {
   const startTime = new Date()
   startTime.setMinutes(startTime.getMinutes() + 5)
+
   const endTime = new Date(startTime)
   endTime.setHours(endTime.getHours() + 1)
+
   return {
     startTime: toLocalISOString(startTime).slice(0, 16),
     endTime: toLocalISOString(endTime).slice(0, 16),
   }
 }
+
 export const AllSlotTypes = [
   SlotType.Bicycle,
   SlotType.Bike,
   SlotType.Car,
   SlotType.Heavy,
 ]
+
 export const formDefaultValuesSearchGarages: DefaultValues<FormTypeSearchGarage> =
   {
     pricePerHour: [0, 200],
@@ -66,6 +70,7 @@ export const formDefaultValuesSearchGarages: DefaultValues<FormTypeSearchGarage>
     length: [0, 100],
     types: AllSlotTypes.sort(),
   }
+
 export const FormProviderSearchGarage = ({
   children,
 }: {
@@ -80,5 +85,6 @@ export const FormProviderSearchGarage = ({
       endTime,
     },
   })
+
   return <FormProvider {...methods}>{children}</FormProvider>
 }
