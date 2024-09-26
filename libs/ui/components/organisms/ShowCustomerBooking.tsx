6793a25a64770e1114af3ad0abf5c011c096ccb1
Nischay <nischay.dtu@gmail.com>
2024-09-26 22:00:46 +0530

    lint

--- libs/ui/components/organisms/ShowCustomerBooking.tsx
+++ libs/ui/components/organisms/ShowCustomerBooking.tsx
@@ -1,6 +1,6 @@
 import {
-    BookingStatus,
-    BookingsForCustomerDocument,
+  BookingStatus,
+  BookingsForCustomerDocument,
 } from '@autospace/network/src/gql/generated'
 import { useTakeSkip } from '@autospace/util/hooks/pagination'
 import { useSession } from 'next-auth/react'
@@ -9,40 +9,40 @@ import { useEffect } from 'react'
 import { ShowData } from './ShowData'
 import { CustomerBookingCard } from './CustomerBookingCard'
 export const ShowCustomerBookings = ({
-    statuses,
+  statuses,
 }: {
-    statuses: BookingStatus[]
+  statuses: BookingStatus[]
 }) => {
-    const session = useSession()
-    const uid = session.data?.user?.uid
-    const { setSkip, setTake, skip, take } = useTakeSkip()
-    const { loading, data, error } = useQuery(BookingsForCustomerDocument, {
-        variables: {
-            skip,
-            take,
-            where: {
-                status: {
-                    in: statuses,
-                },
-            },
+  const session = useSession()
+  const uid = session.data?.user?.uid
+  const { setSkip, setTake, skip, take } = useTakeSkip()
+  const { loading, data, error } = useQuery(BookingsForCustomerDocument, {
+    variables: {
+      skip,
+      take,
+      where: {
+        status: {
+          in: statuses,
         },
-    })
-    return (
-        <ShowData
-            error={error?.message}
-            loading={loading}
-            pagination={{
-                skip,
-                take,
-                resultCount: data?.bookingsForCustomer.length || 0,
-                totalCount: data?.bookingsCount.count || 0,
-                setSkip,
-                setTake,
-            }}
-        >
-            {data?.bookingsForCustomer.map((booking) => (
-                <CustomerBookingCard key={booking.id} booking={booking} />
-            ))}
-        </ShowData>
-    )
+      },
+    },
+  })
+  return (
+    <ShowData
+      error={error?.message}
+      loading={loading}
+      pagination={{
+        skip,
+        take,
+        resultCount: data?.bookingsForCustomer.length || 0,
+        totalCount: data?.bookingsCount.count || 0,
+        setSkip,
+        setTake,
+      }}
+    >
+      {data?.bookingsForCustomer.map((booking) => (
+        <CustomerBookingCard key={booking.id} booking={booking} />
+      ))}
+    </ShowData>
+  )
 }
